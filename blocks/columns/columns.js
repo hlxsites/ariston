6d9517329042469df5401d9c9f40faebd460f2df
Dirk Rudolph <drudolph@adobe.com>
2022-11-16 19:11:11 +0100

    SITES-9519: shadow columns (installation, schema) (#6)
    * feat: add schema columns styling
    * feat: installations columns
    * fix: lint and cards bottom space
    * fix: linting issues

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,13 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+  if (block.matches('.schema')) {
+    const secondaryLink = block.querySelector('.button-container a:nth-child(2)');
+    secondaryLink.insertAdjacentHTML('beforeend', `
+      <svg width="14" height="10" viewBox="0 0 14 10" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+        <use href="/styles/icons.svg#arrow-right"></use>
+      </svg>
+    `);
+  }
 }
